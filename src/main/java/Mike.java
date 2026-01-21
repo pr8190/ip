b72582d699536bf3a6aa35f1ea22daa462fe47bf
pr8190 <E1475630@u.nus.edu>
2026-01-21 21:18:07 +0800

    first commit

---
+++ src/main/java/Mike.java
@@ -0,0 +1,10 @@
+public class Mike {
+    public static void main(String[] args) {
+        String logo = " ___  ___ __ __            \n"
+                + "|   \\/   |__|  | __ ____ \n"
+                + "|  |\\/|  |__|  |/  / __ \\ \n"
+                + "|  |  |  |  |     < ____/\n"
+                + "|__|  |__|__|__|\\__\\____|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
