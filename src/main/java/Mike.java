0a11a76b4916cbb871db770c97aa75866cffea1f
pr8190 <E1475630@u.nus.edu>
2026-01-21 22:07:07 +0800

    level-1

--- src/main/java/Mike.java
+++ src/main/java/Mike.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Mike {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = " ___  ___ __ __            \n"
                 + "|   \\/   |__|  | __ ____ \n"
                 + "|  |\\/|  |__|  |/  / __ \\ \n"
@@ -7,6 +10,16 @@ public class Mike {
                 + "|__|  |__|__|__|\\__\\____|\n";
         System.out
                 .println("Hello! I'm\n" + logo
-                        + "\n-------------------------------------\nBye. Hope to see you again soon!");
+                        + "\nWhat can I do for you?\n-------------------------------------\n");
+        while (true) {
+            String temp = sc.next();
+            if (temp.equals("bye")) {
+                System.out.println(
+                        "-------------------------------------\nBye. Hope to see you again soon!\n-------------------------------------\n");
+                break;
+            }
+            System.out.println(
+                    "-------------------------------------\n" + temp + "\n-------------------------------------\n");
+        }
     }
 }
