8941b2254ced0595b22f942bdbaa66bd44229996
pr8190 <E1475630@u.nus.edu>
2026-01-22 16:58:01 +0800

    Level-3

--- src/main/java/Mike.java
+++ src/main/java/Mike.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Mike {
@@ -11,26 +12,40 @@ public class Mike {
         System.out
                 .println("Hello! I'm\n" + logo
                         + "\nWhat can I do for you?\n-------------------------------------\n");
-        StringBuilder str = new StringBuilder();
-        int c = 1;
+        ArrayList<Task> str = new ArrayList<Task>();
         while (true) {
             String temp = sc.nextLine();
+            String[] tr = temp.split(" ");
             if (temp.equals("bye")) {
                 System.out.println(
                         "-------------------------------------\nBye. Hope to see you again soon!\n-------------------------------------\n");
                 break;
             } else if (temp.equals("list")) {
-                System.out.println("-------------------------------------\n" + //
-                        str.toString() + //
-                        "-------------------------------------\n" + //
+                System.out.println("-------------------------------------\nHere are the tasks in your list: \n");
+                for (int i = 0; i < str.size(); i++)
+                    System.out.println((i + 1) + ". " + str.get(i) + "\n");
+                System.out.println("-------------------------------------\n");
+            } else if (tr[0].equals("mark")) {
+                int k = Integer.parseInt(tr[1]);
+                str.get(k - 1).isDone = true;
+                System.out.println("-------------------------------------\nNice! I've marked this task as done: \n" + //
+                        str.get(k - 1) + //
+                        "\n-------------------------------------\n" + //
                         "");
+            } else if (tr[0].equals("unmark")) {
+                int k = Integer.parseInt(tr[1]);
+                str.get(k - 1).isDone = false;
+                System.out.println(
+                        "-------------------------------------\nOK, I've marked this task as not done yet: \n" + //
+                                str.get(k - 1) + //
+                                "\n-------------------------------------\n" + //
+                                "");
             } else {
                 System.out.println("-------------------------------------\nadded: " + //
                         temp + //
                         "\n-------------------------------------\n" + //
                         "");
-                str.append(c + ". " + temp + "\n");
-                c++;
+                str.add(new Task(temp));
             }
         }
     }
