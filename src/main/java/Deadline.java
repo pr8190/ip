8883415e2114d9b063e4993f7bd62a44cfec78ba
pr8190 <E1475630@u.nus.edu>
2026-01-22 19:00:22 +0800

    A-TextUiTesting

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task {
+    String deadline;
+
+    Deadline(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString()
+                + " (by: " + deadline + ")";
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,16 @@
+public class Event extends Task {
+    String start;
+    String end;
+
+    Event(String description, String start, String end) {
+        super(description);
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString()
+                + " (from: " + start + " to: " + end + ")";
+    }
+}
