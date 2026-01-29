2ac33b7a4774f6cf4a2b89a37d489790b8f37cb7
pr8190 <E1475630@u.nus.edu>
2026-01-29 11:25:05 +0800

    Update Mike.java

--- src/main/java/Mike.java
+++ src/main/java/Mike.java
@@ -1,99 +1,67 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class Mike {
     protected static String fileName = "./src/main/java/data.txt";
+    protected Ui ui;
+    protected Storage storage;
+    protected TaskList taskList;
 
-    public static ArrayList<Task> readFromFile() throws FileNotFoundException {
-        ArrayList<Task> tasks = new ArrayList<>();
-        try {
-            File f = new File(fileName);
-            Scanner sc = new Scanner(f);
-            while (sc.hasNextLine()) {
-                Task temTask = Task.taskFactory(sc.nextLine());
-                tasks.add(temTask);
-            }
-            sc.close();
-            return tasks;
-        } catch (FileNotFoundException e) {
-            System.out.println("File Not Found");
-            return tasks;
-        }
-
+    public Mike(String fileName) {
+        this.ui = new Ui();
+        this.storage = new Storage(fileName);
+        taskList = new TaskList(storage.readFromFile(), ui);
     }
 
-    public static void writeToFile(ArrayList<Task> tasks) throws IOException {
-        try {
-            FileWriter fw = new FileWriter(fileName);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < tasks.size(); i++) {
-                if (i == 0)
-                    sb.append(tasks.get(i).stringDescription());
-                else
-                    sb.append(System.lineSeparator() + tasks.get(i).stringDescription());
+    public void run() {
+        ui.showWelcome();
+        while (true) {
+            String command = ui.readCommand();
+            // ArrayList<Task> tasks = taskList.loadTasks();
+            String[] commandSplit = command.split(" ");
+            if (command.equals("bye")) { // When user inputs 'bye'
+                storage.writeToFile(taskList.loadTasks());
+                ui.showLine();
+                ui.showMessage("Bye. Hope to see you again soon!");
+                ui.showLine();
+                ui.close();
+                break;
+            } else if (command.equals("list")) { // when user inputs 'list' and it lists out all the task in the
+                                                 // arraylist
+                taskList.list();
+            } else if (commandSplit[0].equals("mark")) { // when user asks to mark a particular task as done
+                taskList.mark(command);
+            } else if (commandSplit[0].equals("unmark")) { // when user asks to unmark a particular task
+                taskList.unmark(command);
+            } else if (commandSplit[0].equals("delete")) { // when user asks to delete a particular task from the list
+                taskList.delete(command);
+            } else if (commandSplit[0].equals("todo") || commandSplit[0].equals("deadline")
+                    || commandSplit[0].equals("event")) { // classifying tasks as todo, deadline or event and handling
+                                                          // errors
+                if (commandSplit.length <= 1) {
+                    ui.showLine();
+                    ui.showMessage("OOPS!!! The description of a " + commandSplit[0]
+                            + " cannot be empty.\n");
+                    ui.showLine();
+                    continue;
+                }
+                Task temTask = Task.classifyTask(command);
+                if (temTask == null)
+                    continue;
+                taskList.add(temTask);
+                ui.showLine();
+                ui.showMessage("Got it. I've added this task:\n" + //
+                        temTask + "\nNow you have " + taskList.size() + " tasks in the list.\n");
+                ui.showLine();
+            } else {
+                ui.showLine();
+                ui.showMessage("OOPS!!! I'm sorry, but I don't know what that means :-(\n");
+                ui.showLine();
             }
-            fw.write(sb.toString());
-            fw.close();
-        } catch (IOException e) {
-            System.out.println("File Not Found");
         }
-
     }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        Scanner sc = new Scanner(System.in);
-        String logo = " ___  ___ __ __            \n"
-                + "|   \\/   |__|  | __ ____ \n"
-                + "|  |\\/|  |__|  |/  / __ \\ \n"
-                + "|  |  |  |  |     < ____/\n"
-                + "|__|  |__|__|__|\\__\\____|\n";
-        System.out
-                .println("Hello! I'm\n" + logo
-                        + "\nWhat can I do for you?\n-------------------------------------\n");
-        ArrayList<Task> tasks = readFromFile();
-        while (true) {
-            String temp = sc.nextLine();
-            String[] tr = temp.split(" ");
-            if (temp.equals("bye")) { // When user inputs 'bye'
-                writeToFile(tasks);
-                sc.close();
-                System.out.println(
-                        "-------------------------------------\nBye. Hope to see you again soon!\n-------------------------------------\n");
-                sc.close();
-                break;
-            } else if (temp.equals("list")) { // when user inputs 'list' and it lists out all the task in the arraylist
-                Update.list(tasks);
-            } else if (tr[0].equals("mark")) { // when user asks to mark a particular task as done
-                Update.mark(tasks, temp);
-            } else if (tr[0].equals("unmark")) { // when user asks to unmark a particular task
-                Update.unmark(tasks, temp);
-            } else if (tr[0].equals("delete")) { // when user asks to delete a particular task from the list
-                Update.delete(tasks, temp);
-            } else { // classifying tasks as todo, deadline or event and handling errors
-                if (tr.length <= 1 && (tr[0].equals("todo") || tr[0].equals("deadline") || tr[0].equals("event"))) {
-                    System.out.println("-------------------------------------\nOOPS!!! The description of a " + tr[0]
-                            + " cannot be empty.\n-------------------------------------\n");
-                    continue;
-                } else if (tr.length <= 1) {
-                    System.out.println(
-                            "-------------------------------------\nOOPS!!! I'm sorry, but I don't know what that means :-(\n-------------------------------------\n");
-                    continue;
-                }
-                Task temTask = Task.classifyTask(temp);
-                if (temTask == null)
-                    continue;
-                tasks.add(temTask);
-                System.out.println("-------------------------------------\nGot it. I've added this task:\n" + //
-                        temTask + //
-                        "\nNow you have " + tasks.size()
-                        + " tasks in the list.\n-------------------------------------\n"
-                        + //
-                        "");
-            }
-        }
+        new Mike(fileName).run();
     }
 }
